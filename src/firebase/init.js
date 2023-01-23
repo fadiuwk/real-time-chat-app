894c7e8136176f15e06de364e6430024c9313102
Fade Fares <52964451+fadiuwk@users.noreply.github.com>
2023-01-23 20:34:08 +0200

    Update init.js

--- src/firebase/init.js
+++ src/firebase/init.js
@@ -2,13 +2,13 @@ import firebase from "firebase/app";
 require("firebase/firestore");
 // Your web app's Firebase configuration
 var firebaseConfig = {
-  apiKey: "...",
-  authDomain: "...",
-  databaseURL: "...",
-  projectId: "...",
-  storageBucket: "",
-  messagingSenderId: "...",
-  appId: "..."
+  apiKey: "AIzaSyCHy9bKa874i9Yihjo6_g8whShUFqSKBlg",
+  authDomain: "chat-3ac69.firebaseapp.com",
+  projectId: "chat-3ac69",
+  storageBucket: "chat-3ac69.appspot.com",
+  messagingSenderId: "847693758753",
+  appId: "1:847693758753:web:72c6c99ab282172416be60",
+  measurementId: "G-PSGWQFT55X"
 };
 
 /*
